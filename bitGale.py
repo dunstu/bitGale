d443defccd231039e4fcb5122de09a3c1727b55b
dunstu <duncanstuartcanada@gmail.com>
2017-02-16 18:16:20 -0500

    Added image opening

--- bitGale.py
+++ bitGale.py
@@ -1,1 +1,12 @@
-#TODO
+from PIL import Image
+
+imgReadOK = False
+while not imgReadOK:
+    try:
+        imgDir = input("Path location of image to open:")
+        userImg = Image.open(imgDir)
+        imgReadOK = True
+    except IOError:
+        print("File not found!")
+
+userImg.show()
