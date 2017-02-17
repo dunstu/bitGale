92f4e132dad4f06a20f516cc049cce5c050933bf
dunstu <duncanstuartcanada@gmail.com>
2017-02-16 19:47:52 -0500

    Put open file code into function definition

--- bitGale.py
+++ bitGale.py
@@ -1,12 +1,15 @@
 from PIL import Image
 
-imgReadOK = False
-while not imgReadOK:
-    try:
-        imgDir = input("Path location of image to open:")
-        userImg = Image.open(imgDir)
-        imgReadOK = True
-    except IOError:
-        print("File not found!")
-
-# userImg.show()
+def open_image():
+    '''
+    Prompt user for directory, open image file (if found) and return
+    '''
+    imgReadOK = False
+    while not imgReadOK:
+        try:
+            imgDir = input("Path location of image to open:")
+            userImg = Image.open(imgDir)
+            imgReadOK = True
+        except IOError:
+            print("File not found!")
+    return userImg
