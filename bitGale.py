e395288235d6a836b67758a69807ab37058ee085
dunstu <duncanstuartcanada@gmail.com>
2017-02-17 17:55:04 -0500

    In open_image(): changed return from 'image' object to an array. Also, added main()

--- bitGale.py
+++ bitGale.py
@@ -2,14 +2,26 @@ from PIL import Image
 
 def open_image():
     '''
-    Prompt user for directory, open image file (if found) and return
+    Prompt user for directory, open image file (if found) and return as an array
+    Directory for testing: D:\PyCharm\Projects\bitGale\testImg.jpg
     '''
     imgReadOK = False
     while not imgReadOK:
         try:
-            imgDir = input("Path location of image to open:")
+            # imgDir = input("Path location of image to open:")  # Commented out for testing purposes
+            imgDir = 'D:\\PyCharm\\Projects\\bitGale\\testImg.jpg'
             userImg = Image.open(imgDir)
             imgReadOK = True
         except IOError:
             print("File not found!")
-    return userImg
+
+        rawPixels = list(userImg.getdata())
+        width, height = userImg.size
+        imgAsArray = [rawPixels[i * width:(i + 1) * width] for i in range(height)]
+    return imgAsArray
+
+
+def main():
+    userImage = open_image()
+
+main()
