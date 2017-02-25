fe36a08880978e97a1b4a996f6d9bd7433bfe902
dunstu <duncanstuartcanada@gmail.com>
2017-02-25 13:51:48 -0500

    added quit command, prompt to save unsaved changes before quitting

--- bitGale.py
+++ bitGale.py
@@ -9,6 +9,7 @@ class bitGaleShell(cmd.Cmd):
     prompt = 'bitGale~: '
     file = None
     imageArray = []
+    savedLastChange = False
 
     def preloop(self):
         self.imageArray = imgio.make_pixel_array(imgio.open_image())
@@ -20,16 +21,29 @@ class bitGaleShell(cmd.Cmd):
     def do_sort(self, rawInput):
         flags = imgio.parse(rawInput)
         self.imageArray = effects.pixel_sort(self.imageArray, flags)
+        self.savedLastChange = False
 
     def do_rgboff(self, rawInput):
         flags = imgio.parse(rawInput)
         self.imageArray = effects.rgb_offset(self.imageArray, flags)
+        self.savedLastChange = False
 
     def do_save(self, rawInput):
         imgio.save_image(imgio.make_pil_image(self.imageArray))
+        self.savedLastChange = True
 
     def do_show(self, rawInput):
         imgio.make_pil_image(self.imageArray).show()
+
+    def do_quit(self, rawInput):
+        while not self.savedLastChange:  # If effects have been applied since last save, ask user to save
+            okToQuit = input("You have some unsaved changes. Do you still want to quit?(y,n)")
+            if okToQuit == 'y':
+                print("Exiting bitGale...")
+                return True
+            else:
+                print('Returning to shell.')
+                return False
 
 if __name__ == '__main__':
     bitGaleShell().cmdloop()
